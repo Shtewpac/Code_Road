980deaca23426a228f2f9e645c5258b7438024ea
Will Kraft <wpkfilms@gmail.com>
2024-06-13 16:16:54 -0700

    Refactor Dinosaur Game code to implement Q-learning algorithm

--- Live Dialogue Assistant Web App/development_utils/print_directory_tree.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def print_tree(path, indent=''):
-    print(indent + os.path.basename(path) + '/')
-    if os.path.isdir(path):
-        for filename in os.listdir(path):
-            print_tree(os.path.join(path, filename), indent + '  ')
-
-print_tree('.')
